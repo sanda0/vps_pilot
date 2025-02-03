ed3892759f740fac3db1e8eed9c6f7605c4f4ba4
sandakelu-dev <sandakelum.dev@gmail.com>
2025-02-03 22:55:07 +0530

    add TCP server implementation, create node_sys_info and node_disk_info tables, and update node table migration

---
+++ server/sql/migrations/20250203171934_node_sys_info_table.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS node_sys_info (
+  id SERIAL PRIMARY KEY,
+  node_id INTEGER NOT NULL,
+  os TEXT,
+  platform TEXT,
+  platform_version TEXT,
+  kernel_version TEXT,
+  cpus TEXT,
+  total_memory TEXT,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (node_id) REFERENCES nodes (id) ON DELETE CASCADE
+);
--- server/tcp_server/server.go
+++ server/tcp_server/server.go
@@ -1,5 +1,36 @@
 package tcpserver
 
-func StartTcpServer() {
-	// Start the TCP server
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/sanda0/vps_pilot/db"
+)
+
+var AgentConnections map[string]net.Conn
+
+func StartTcpServer(ctx context.Context, repo *db.Repo, port string) {
+	AgentConnections = make(map[string]net.Conn)
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		return
+	}
+	defer listener.Close()
+	fmt.Println("TCP server Listening on port", port)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return
+		}
+		AgentConnections[conn.RemoteAddr().String()] = conn
+		go handleRequest(ctx, repo, conn)
+	}
+}
+
+func handleRequest(ctx context.Context, repo *db.Repo, conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("New connection from", conn.RemoteAddr())
+
 }
