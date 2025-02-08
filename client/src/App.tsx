ecdbca266cade38e7480496ce776a670bcd0f52d
sandakelu-dev <sandakelum.dev@gmail.com>
2025-02-08 21:58:29 +0530

    feat: add theme provider and toggle functionality to manage light/dark mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from "react-router";
 import router from "./router/router";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
 
   return (
     <>
-      <RouterProvider router={router}></RouterProvider>
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <RouterProvider router={router}></RouterProvider>
+      </ThemeProvider>
     </>
   )
 }
