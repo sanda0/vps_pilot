9cde58997137bf5cf6fd19749216c06b0340cbe8
sanda0 <sandkelum.work@gmail.com>
2026-03-07 20:12:23 +0530

    fix ui configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +10,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: "../server/cmd/app/dist",
+    emptyOutDir: true,
+  },
+});
